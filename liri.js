03f3bb81145bc0f3600289a715010aab5bd1c183
Nan Wu <32279235+virus0071@users.noreply.github.com>
2018-07-05 16:38:11 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
 
 var fs = require("fs");
-var action = process.argv[2];
-var value = process.argv[3];
-var Twitter = require("twitter");
-var spotify = require("spotify");
-var keys = require("keys.js");
+var keys = require("./keys.js");
 var request = require("request");
 
+var action = process.argv[2];
+var value = process.argv[3];
+
+var Twitter = require("twitter");
 var client = new Twitter(keys.twitter);
-var params = {screen_name: "virus00712"};
+
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+
+
+
+
+
+
 
 switch (action) {
     case "my-tweets":
@@ -34,33 +43,40 @@ switch (action) {
 
 
 function tweets() {
-    client.get("statuses/user_timeline/", params, function (error, tweets, response) {
-        if (!error && response.statusCode == 200){
-            fs.appendFile("log.txt", "**************************************************** \r\n", function(err){
-                if (err) {
-                    throw err;
-                }
-            });
-            console.log(" ");
-            console.log("Last 20 Tweets: ");
-            for (i = 0; i < 19; i++){
-                var number = i++;
-                var tResult = number + tweets[i].text + "\r\n" + tweets[i].created_at + "\r\n" +
-                    "***********************" +i +"***************************** \r\n";
-                console.log(tResult);
-            }
-    
-    
-    fs.appendFile("log.txt", ("**************************************************** \r\n" + 
-        tweets[i].text + "\r\n" +
-        tweets[i].created_at + "\r\n" +
-        "***********************" +i +"***************************** \r\n"), function(err,data){
-        if (err) {
+    var params = {
+        screen_name: "Nan"
+    } && {
+        count: 20
+    };
+
+    client.get("statuses/user_timeline/", params, function (error, data, response) {
+        
+        
+        if (!error && response.statusCode === 200){
+            for(var i = 0; i < 10; i++) {
+                
+            var tResults = 
+			data[i].user.name + ": \r\n" +
+			data[i].text + "\r\n" + 
+			data[i].created_at + "\r\n" + 
+            "****************************************************" + "\r\n";
+            
+            console.log(tResults);
+            fs.appendFile("log.txt", ("****************************************************" + "\r\n" +
+            data[i].user.name + "\r\n" +
+            data[i].text + "\r\n" + 
+			data[i].created_at + "\r\n" + 
+            "****************************************************" + "\r\n"), function(err,data){
+            if (err) {
             return console.log(err);
         }
+        
     });
-}
+        }
+    }
+            
 });
+    
 }
 
 
@@ -70,7 +86,7 @@ function omdbThisMovie(value) {
     if (value == null) {
         value = "Mr. Nobody";
     }
-    request ("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    request ("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
         if (!error && response.statusCode === 200) {
             console.log("****************************************************");
             console.log("Title: " + JSON.parse(body).Title);
@@ -83,7 +99,7 @@ function omdbThisMovie(value) {
             console.log("Actors: " + JSON.parse(body).Actors);
             console.log("****************************************************");
 
-            fs.append("log.txt", "**************************************************** \r\n" +
+            fs.appendFile("log.txt", "**************************************************** \r\n" +
             "Title: " + JSON.parse(body).Title + "\r\n" +
             "Year: " + JSON.parse(body).Year + "\r\n" +
             "IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n" +
@@ -110,25 +126,34 @@ function spotifyThisSong(value){
     if (value == null){
         value = "Walk this way";
     }
-    spotify.search({type: "track", query: value}, function (error, body, response){
-        if (!error && response.statusCode == 200) {
+    
+    spotify.search({ type: "track", query: value }, function (error, body, response){
+        if(error){
+            console.log(
+                error
+            )
+        }
+        if (!error) {
+            
             console.log("****************************************************");
-            console.log("Artist(s): " + JSON.parse(body).tracks.items[0].artists[0].name);
-            console.log("Song Name:  " + JSON.parse(body).tracks.items[0].name);
-            console.log("Preview Link: " + JSON.parse(body).tracks.items[0].preview_url);
-            console.log("Album: " + JSON.parse(body).tracks.items[0].album.name);
+            console.log("Artist(s): " + body.tracks.items[0].artists[0].name);
+            console.log("Song Name:  " + body.tracks.items[0].name);
+            console.log("Preview Link: " + body.tracks.items[0].preview_url);
+            console.log("Album: " + body.tracks.items[0].album.name);
             console.log("****************************************************");
 
             fs.appendFile("log.txt", "**************************************************** \r\n" +
-            "Artist(s): " + JSON.parse(body).tracks.items[0].artists[0].name + "\r\n" +
-            "Song Name:  " + JSON.parse(body).tracks.items[0].name + "\r\n" +
-            "Preview Link: " + JSON.parse(body).tracks.items[0].preview_url + "\r\n" +
-            "Album: " + JSON.parse(body).tracks.items[0].album.name + "\r\n" + 
+            "Artist(s): " + body.tracks.items[0].artists[0].name + "\r\n" +
+            "Song Name:  " + body.tracks.items[0].name + "\r\n" +
+            "Preview Link: " + body.tracks.items[0].preview_url + "\r\n" +
+            "Album: " + body.tracks.items[0].album.name + "\r\n" + 
             "****************************************************" + "\r\n", function (err){
                 if (err) {
-                    throw err;
-                };
+                    console.log(err);
+        };
+                
             })
+        
         }
     })
 }
@@ -139,9 +164,9 @@ function random() {
             console.log(error);
         } else {
             rResult = data.split(",");
-            if (rResult[0] === "spotify-this-song"){
+            if (rResult[0] === "song"){
                 spotifyThisSong(rResult[1]);
-            } if (rResult[0] === "omdb-this-movie"){
+            } if (rResult[0] === "movie"){
                 omdbThisMovie(rResult[1]);
             }
         };
