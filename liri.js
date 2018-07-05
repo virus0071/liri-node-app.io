0f9a2be68c25b605da10199a3e9c17db3bac158f
Nan Wu <32279235+virus0071@users.noreply.github.com>
2018-07-05 16:38:36 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -22,22 +22,22 @@ var spotify = new Spotify(keys.spotify);
 
 switch (action) {
     case "my-tweets":
-    tweets();
-    break;
-    
+        tweets();
+        break;
+
     case "spotify":
-    spotifyThisSong(value);
-    break;
+        spotifyThisSong(value);
+        break;
 
     case "movie":
-    omdbThisMovie(value);
-    break;
+        omdbThisMovie(value);
+        break;
 
     case "do-what-it-says":
-    random();
-    break;
+        random();
+        break;
 
-    
+
 }
 
 
@@ -46,37 +46,37 @@ function tweets() {
     var params = {
         screen_name: "Nan"
     } && {
-        count: 20
-    };
+            count: 20
+        };
 
     client.get("statuses/user_timeline/", params, function (error, data, response) {
-        
-        
-        if (!error && response.statusCode === 200){
-            for(var i = 0; i < 10; i++) {
-                
-            var tResults = 
-			data[i].user.name + ": \r\n" +
-			data[i].text + "\r\n" + 
-			data[i].created_at + "\r\n" + 
-            "****************************************************" + "\r\n";
-            
-            console.log(tResults);
-            fs.appendFile("log.txt", ("****************************************************" + "\r\n" +
-            data[i].user.name + "\r\n" +
-            data[i].text + "\r\n" + 
-			data[i].created_at + "\r\n" + 
-            "****************************************************" + "\r\n"), function(err,data){
-            if (err) {
-            return console.log(err);
+
+
+        if (!error && response.statusCode === 200) {
+            for (var i = 0; i < 10; i++) {
+
+                var tResults =
+                    data[i].user.name + ": \r\n" +
+                    data[i].text + "\r\n" +
+                    data[i].created_at + "\r\n" +
+                    "****************************************************" + "\r\n";
+
+                console.log(tResults);
+                fs.appendFile("log.txt", ("****************************************************" + "\r\n" +
+                    data[i].user.name + "\r\n" +
+                    data[i].text + "\r\n" +
+                    data[i].created_at + "\r\n" +
+                    "****************************************************" + "\r\n"), function (err, data) {
+                        if (err) {
+                            return console.log(err);
+                        }
+
+                    });
+            }
         }
-        
+
     });
-        }
-    }
-            
-});
-    
+
 }
 
 
@@ -86,7 +86,7 @@ function omdbThisMovie(value) {
     if (value == null) {
         value = "Mr. Nobody";
     }
-    request ("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
             console.log("****************************************************");
             console.log("Title: " + JSON.parse(body).Title);
@@ -100,19 +100,19 @@ function omdbThisMovie(value) {
             console.log("****************************************************");
 
             fs.appendFile("log.txt", "**************************************************** \r\n" +
-            "Title: " + JSON.parse(body).Title + "\r\n" +
-            "Year: " + JSON.parse(body).Year + "\r\n" +
-            "IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n" +
-            "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating + "\r\n" + 
-            "Country: " + JSON.parse(body).Country + "\r\n" +
-            "Language: " + JSON.parse(body).Language + "\r\n" +
-            "Plot: " + JSON.parse(body).Plot + "\r\n" +
-            "Actors: " + JSON.parse(body).Actors + "\r\n" +
-            "****************************************************" + "\r\n", function(error){
-                if (error) {
-                    throw error;
-                };
-            })
+                "Title: " + JSON.parse(body).Title + "\r\n" +
+                "Year: " + JSON.parse(body).Year + "\r\n" +
+                "IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n" +
+                "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating + "\r\n" +
+                "Country: " + JSON.parse(body).Country + "\r\n" +
+                "Language: " + JSON.parse(body).Language + "\r\n" +
+                "Plot: " + JSON.parse(body).Plot + "\r\n" +
+                "Actors: " + JSON.parse(body).Actors + "\r\n" +
+                "****************************************************" + "\r\n", function (error) {
+                    if (error) {
+                        throw error;
+                    };
+                })
         }
     })
 
@@ -122,19 +122,19 @@ function omdbThisMovie(value) {
 
 
 
-function spotifyThisSong(value){
-    if (value == null){
+function spotifyThisSong(value) {
+    if (value == null) {
         value = "Walk this way";
     }
-    
-    spotify.search({ type: "track", query: value }, function (error, body, response){
-        if(error){
+
+    spotify.search({ type: "track", query: value }, function (error, body, response) {
+        if (error) {
             console.log(
                 error
             )
         }
         if (!error) {
-            
+
             console.log("****************************************************");
             console.log("Artist(s): " + body.tracks.items[0].artists[0].name);
             console.log("Song Name:  " + body.tracks.items[0].name);
@@ -143,30 +143,30 @@ function spotifyThisSong(value){
             console.log("****************************************************");
 
             fs.appendFile("log.txt", "**************************************************** \r\n" +
-            "Artist(s): " + body.tracks.items[0].artists[0].name + "\r\n" +
-            "Song Name:  " + body.tracks.items[0].name + "\r\n" +
-            "Preview Link: " + body.tracks.items[0].preview_url + "\r\n" +
-            "Album: " + body.tracks.items[0].album.name + "\r\n" + 
-            "****************************************************" + "\r\n", function (err){
-                if (err) {
-                    console.log(err);
-        };
-                
-            })
-        
+                "Artist(s): " + body.tracks.items[0].artists[0].name + "\r\n" +
+                "Song Name:  " + body.tracks.items[0].name + "\r\n" +
+                "Preview Link: " + body.tracks.items[0].preview_url + "\r\n" +
+                "Album: " + body.tracks.items[0].album.name + "\r\n" +
+                "****************************************************" + "\r\n", function (err) {
+                    if (err) {
+                        console.log(err);
+                    };
+
+                })
+
         }
     })
 }
 
 function random() {
-    fs.readFile("random.txt", "utf8", function(error, data){
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
         } else {
             rResult = data.split(",");
-            if (rResult[0] === "song"){
+            if (rResult[0] === "song") {
                 spotifyThisSong(rResult[1]);
-            } if (rResult[0] === "movie"){
+            } if (rResult[0] === "movie") {
                 omdbThisMovie(rResult[1]);
             }
         };
